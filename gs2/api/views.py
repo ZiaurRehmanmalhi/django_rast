2cc068cd1e02ab40d8d67f2ead162deb078ad4f4
ZiaurRehmanmalhi <ziamalhi1234@gmail.com>
2023-06-04 23:06:45 +0500

    add

--- gs2/api/views.py
+++ gs2/api/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+import io
+from rest_framework.parsers import JSONParser
+from .serializers import StudentSerializer
+from rest_framework.renderers import JSONRenderer
+from django.http import HttpResponse
+
 
 # Create your views here.
+def student_create(request):
+    if request.method == 'POST':
+        json_data = request.body
+        stream = io.BytesIO(json_data)
+        pythondata = JSONParser().parse(stream)
+        serializer = StudentSerializer(pythondata)
+        if serializer.is_valid():
+            serializer.save()
+            res = {'msg':'Data Created'}
+            json_data = JSONRenderer().render(msg)
+            return HttpResponse(json_data, content_type='application/json')
+
+        json_data = JSONRenderer().render(serializer.errors)
+        return HttpResponse(json_data, content_type='application/json')
